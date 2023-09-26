fd39216debe0565a60a2f751e00fa2557227f0d0
Ayush Jain <2022bcacsbayush12896@poornima.edu.in>
2023-09-27 00:55:00 +0530

    Update

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -3,13 +3,29 @@ import 'package:E_commerce_app/pages/widgets/drawer.dart';
 import 'package:E_commerce_app/pages/widgets/items_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-class YourHomePage extends StatelessWidget {
+class YourHomePage extends StatefulWidget {
   // const MyWidget({super.key});
 
  
 
   YourHomePage({super.key});
+
   @override
+  State<YourHomePage> createState() => _YourHomePageState();
+}
+
+class _YourHomePageState extends State<YourHomePage> {
+  @override
+
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData(){
+    
+  }
+
   Widget build(BuildContext context) {  
     return Scaffold(
      appBar: AppBar(elevation: 0.00, surfaceTintColor: Colors.black,shadowColor: Colors.black,backgroundColor: Colors.white,
--- lib/pages/models/catalog.dart
+++ lib/pages/models/catalog.dart
@@ -18,11 +18,11 @@ static final items= [
 Item(
   id: 1,  
   name : "Iphone 12 Pro",
-  desc : "Apple Iphone 12 Pro",
+  desc : "Apple iPhone 12th generation",
   price: "70000/-",
-  color :"33505a",
+  color :"#33505a",
   image : 
-          "https://rukminim2.flixcart.com/image/416/416/kg8avm80/mobile/b/v/a/apple-iphone-12-pro-dummyapplefsn-original-imafwgbr45csnczy.jpeg?q=70"
+          "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-12-pro-blue-hero?wid=940&hei=1112&fmt=png-alpha&qlt=80&.v=1604021661000"
   )
  ];
 }
